7a8b440297ad5b7b3ceecb92b6d2f22f450b3e66
Dileep Kanumuri <dileep@blueteaklabs.com>
2020-05-07 21:23:16 +0530

    corrected footer import in layout component

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,6 +1,6 @@
 import React from "react"
 import Header from "../components/header"
-import Footer from "../components/Footer"
+import Footer from "../components/footer"
 import "../styles/index.scss"
 import layoutStyles from "./layout.module.scss"
 
